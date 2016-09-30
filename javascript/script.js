5d06897fc886d0660f252f692ff2ef7de3366fbc
Nia Thomas <thomn705@newschool.edu>
2016-09-30 16:42:08 +0000

    around

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+console.log("Some nonsense");
+
+var timer = 0;
+var counter = 0;
+
+for (var i = 0; i < 1000; i++) {
+    setTimeout(function() {
+        var nonsense = "";
+        for (var s = 0; s < 40; s++) {
+            if (Math.random()*100 < 40) {
+                nonsense += "blah blah";
+            } else {
+                nonsense += "";
+            }
+    
+        }
+        console.log(nonsense);
+    }, timer);
+    timer += 1000;
+    counter++;
+}
